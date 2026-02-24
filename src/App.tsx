ef66158a903b6187ff686fb0d81bd4babc9aa9f4
zeynepeceyunkul <eceyunkul44@gmail.com>
2026-02-24 13:34:00 +0300

    chore: initial project setup with Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarımı ve Programlama</h1>
+      <h2>LAB - 1</h2>
+      <p>Ad Soyad: Zeynep Ece Yünkül</p>
+      <p>Öğrenci No: 235542004</p>
+    </div>
+  )
+}
+
+export default App
