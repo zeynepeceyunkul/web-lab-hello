ee2875c7b2560fa57eb09301a06e659319af482b
zeynepeceyunkul <eceyunkul44@gmail.com>
2026-02-24 13:35:57 +0300

    feat: add personal details to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ function App() {
       <h2>LAB - 1</h2>
       <p>Ad Soyad: Zeynep Ece Yünkül</p>
       <p>Öğrenci No: 235542004</p>
+      <p>Bölüm: Yazılım Mühendisliği</p>
+      <p>Kısa Tanıtım: Frontend öğreniyorum 🚀</p>
     </div>
   )
 }
